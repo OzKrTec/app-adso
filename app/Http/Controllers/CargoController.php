08f32054e11aae54450c4c5192115aeb58a6aa19
OzKr <ozkr.sierrae@gmail.com>
2024-08-28 21:32:03 -0500

    Se crearon metodos y rutas de los modelos caqrgos y dptos del proyecto

--- app/Http/Controllers/CargoController.php
+++ app/Http/Controllers/CargoController.php
@@ -8,4 +8,48 @@ use Illuminate\Http\Request;
 class CargoController extends Controller
 {
     //
+
+    public function getData(Request $request){
+
+        $rta= 10+20;
+        return response()->json([
+            'status' => '200',
+            'message' => 'data..',
+            'result'=> $rta
+        ]);
+    }
+
+    public function save(Request $request){
+
+        if($request->edad>=18){
+            $rta=' Mayor de edad ';
+            return $request->nombre . $rta;
+        }
+
+        return response()->json([
+            'status' => '200',
+            'message' => 'guardado con exito',
+            'data' => $request->nombre,
+
+        ]);
+    }
+
+    public function update(Request $request){
+
+        return response()->json([
+            'status' => '200',
+            'message' => 'actualizado con exito',
+        ]);
+    }
+
+    public function delete(Request $request){
+
+        return response()->json([
+            'status' => '200',
+            'message' => 'Se elimino con exito',
+        ]);
+    }
+
+
+
 }
